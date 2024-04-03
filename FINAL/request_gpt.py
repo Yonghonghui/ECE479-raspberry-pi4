3482af7b80052d09d3f8797fb0717cd221227f81
haoyuh3 <haoyuh3>
2024-04-03 00:04:47 -0500

    server2

---
+++ FINAL/request_gpt.py
@@ -0,0 +1,54 @@
+
+
+
+import json
+from openai import OpenAI
+
+# get key
+with open("key.txt", "r") as key_file:
+    api_key = key_file.read().strip()
+
+client = OpenAI(api_key=api_key)
+
+# store history
+history_messages = [
+    {"role": "system", "content": "You are a helpful assistant."} ## can be changed
+]
+
+
+def call_openai_api(new_question):
+    global history_messages
+
+    #new_message_content = new_question
+    new_message = {"role": "user", "content": new_question}
+    history_messages.append(new_message)
+
+
+    ## user can change specific model or setting
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        max_tokens=100,
+        messages=history_messages
+    )
+
+    ## store response
+    new_response = {"role": "assistant", "content": response.choices[0].message.content}
+    history_messages.append(new_response)
+    return response
+
+if __name__ == "__main__":
+    message = ""
+    while True:
+        new_question = input("Enter data: ")
+        if(new_question == "quit"):
+            break
+        message += f"User: {new_question}\n"
+        response = call_openai_api(new_question)
+        answer = response.choices[0].message.content
+        print(response.choices[0].message.content)
+        message += f"AI: {answer}\n"
+
+    with open("conversation.txt", "w") as file:
+        file.write(message)
+    
+    
